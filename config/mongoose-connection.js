3f4cd1fdb90e726e0b5d8e13610f84a8e98c441f
DEV-PHADOL <devphadol1919@gmail.com>
2024-06-28 10:18:11 +0530

    created Owner route

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const dbgr = require('debug')('development:mongoose');
+const config = require('config');
 
 mongoose
-.connect("mongodb://localhost:27017/rogue")
+.connect(`${config.get("MONGODB_URI")}/rogue`)
 .then(()=>{
-    console.log("DB CONNECTED");
+    dbgr("DB CONNECTED");
 })
 .catch((err)=>{
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model');
+require('dotenv').config();
 
 router.get('/',(req,res)=>{
     res.send("hey");
 });
+
+if(process.env.NODE_ENV){
+    router.post('/create',async (req,res)=>{
+        let owner = await ownerModel.find();
+        if(owner.length > 0){
+            return res
+            .send(503)
+            .send("You don't have permission to create a new owner");
+        }
+        let{fullname,email,password}=req.body;
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        });
+        res.status(201).send(createdOwner);
+    });
+}
 
 module.exports = router;
