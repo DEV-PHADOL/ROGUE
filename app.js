dd45e0237cf66d31a36d1cb3287fcd80294d057d
DEV-PHADOL <devphadol1919@gmail.com>
2024-07-06 18:45:58 +0530

    authentication

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 const path = require('path');
+require('dotenv').config();
+
 const db = require('./config/mongoose-connection');
 const ownersRouter = require('./routes/ownersRouter');
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
+const index = require('./routes/index');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -14,6 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
+app.use('/',index)
 app.use('/owners',ownersRouter);
 app.use('/products',productsRouter);
 app.use('/users',usersRouter);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const { generateToken } = require('../utils/generateToken');
+const { registerUser } = require('../controllers/authController');
 
 router.get('/',(req,res)=>{
     res.send("hey");
 });
+
+router.post('/register',registerUser);
+
 
 module.exports = router;
