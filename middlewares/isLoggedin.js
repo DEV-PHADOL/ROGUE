b6584f11e77517c5df5cf2e688ffc5f6693a13ab
DEV-PHADOL <devphadol1919@gmail.com>
2024-07-08 20:50:43 +0530

    create product page

--- middlewares/isLoggedin.js
+++ middlewares/isLoggedin.js
@@ -2,20 +2,19 @@ const jwt = require('jsonwebtoken');
 const userModel = require('../models/user-model');
 
 module.exports = async (req,res,next)=>{
-    if(!req.cookie.token){
+    if(!req.cookies.token){
         req.flash('error',"You need to Login First");
         return res.redirect('/');
     }
     try{
-        let decode = jwt.verify(req.cookie.token,process.env.JWT_KEY);
-        let user = await userModel
-        .findOne({ email : decode.email })
-        .select('-password');
-
-        req.user = user;
+        let decode = jwt.verify(req.cookies.token, process.env.JWT_KEY);
+        let use = await userModel
+            .findOne({ email : decode.email })
+            .select("-password");
+        req.user = use;
         next();
     }catch(err){
         req.flash('error',"Something went WRONG");
-        return res.redirect('/');        
+        res.redirect('/');        
     }
 }
